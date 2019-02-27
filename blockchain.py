052dca9244ee2ac8ae0ee8a394a1afbec45eb81e
Adrian Valencia <38024660+adrianv425@users.noreply.github.com>
2019-02-27 12:19:11 -0800

    Update blockchain.py
    fixed small bug causes app.run() to always run

--- blockchain.py
+++ blockchain.py
@@ -734,7 +734,6 @@ def home():
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-
     parser = ArgumentParser()
     parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
     args = parser.parse_args()
