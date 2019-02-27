5d8678ec44aebafe4e45121f0e2b4282f3492e0b
Adrian Valencia <38024660+adrianv425@users.noreply.github.com>
2019-02-27 12:41:17 -0800

    removed app.run()
    needed to remove app.run() so pythonanywhere.com can deploy without errors

--- blockchain.py
+++ blockchain.py
@@ -731,11 +731,3 @@ def home():
 
     return render_template('index.html', chain=chain, nodes=list(blockchain.nodes)), 200
 
-
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-    app.run(host='0.0.0.0', port=port)
